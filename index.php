84be954b00c4a7005e4a92dbaf278208aab65cef
kazarovetss <136485938+kazarovetss@users.noreply.github.com>
2024-04-16 14:05:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $text = $_POST['text'] ?? ''; // Получаем текст из поля 'text'
+    //Работа с базой данных
+    echo "Получен текст: " . htmlspecialchars($text);
+}
+?>
